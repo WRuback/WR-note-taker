7119f25c4cc952923aa319f5582c400b95ec62a3
William Ruback <rubackw@gmail.com>
2022-01-29 16:12:13 -0500

    Added get, post, and delete calls to notes api.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,57 @@
-const notesRouter = require("express").Router();
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
 
-notesRouter.get("/", (req,res) => {
-    res.send("Notes get working.")
+const notesRouter = express.Router();
+
+
+notesRouter.get("/", (req, res) => {
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-notesRouter.post("/", (req,res) => {
-    res.send("Notes post working.")
+notesRouter.post("/", (req, res) => {
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4()
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const output = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(output);
+    }else{
+        res.json('Error in adding note to DB.');
+    }
+});
+
+notesRouter.delete("/:id", (req, res) => {
+    console.log(req.params.id);
+    readFromFile("./db/db.json").then((rawData) => {
+        const data = JSON.parse(rawData);
+        console.log (data);
+        const newNotes = data.filter((element) => {
+            console.log(element.note_id);
+            console.log(req.params.id);
+            return element.note_id !== req.params.id;
+        });
+        console.log(newNotes);
+        if(newNotes !== data){
+            writeToFile("./db/db.json", newNotes);
+            res.json(`Note sucessfully deleted.`);
+        }
+        else{
+            res.json(`Error in deleting note. Could not be found.`);
+        }
+    });
 });
 
 module.exports = notesRouter;
